b15b7a6c22068d39a15e4728c8ca1ba84b0c10a8
robithc <robithc@gmail.com>
2018-05-24 16:14:26 +0300

    upd

---
+++ app/src/main/java/com/example/android/newsapp/News.java
@@ -0,0 +1,20 @@
+package com.example.android.newsapp;
+
+/**
+ * Created by RO221D on 2018-05-24.
+ */
+
+public class News {
+
+    String id;
+    String type;
+    String sectionId;
+    String sectionName;
+    String webPublicationDate;
+    String webTitle;
+    String webUrl;
+    String apiUrl;
+
+
+
+}
---
+++ app/src/main/java/com/example/android/newsapp/NewsAdapter.java
@@ -0,0 +1,8 @@
+package com.example.android.newsapp;
+
+/**
+ * Created by RO221D on 2018-05-24.
+ */
+
+public class NewsAdapter {
+}
