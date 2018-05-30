f2673c3b2739d540dc9e2dd72623673c5afcc1b3
robithc <robithc@gmail.com>
2018-05-30 13:49:50 +0300

    upd

--- app/src/main/java/com/example/android/newsapp/News.java
+++ app/src/main/java/com/example/android/newsapp/News.java
@@ -6,15 +6,65 @@ package com.example.android.newsapp;
 
 public class News {
 
-    String id;
-    String type;
-    String sectionId;
-    String sectionName;
-    String webPublicationDate;
-    String webTitle;
-    String webUrl;
-    String apiUrl;
+    private String mTitle;
+    private String mDate;
+    private String mUrl;
+    private String mAuthor;
+    private String mSection;
 
+    public News(String title, String date, String url, String author, String section) {
 
+        mTitle = title;
+        mDate = date;
+        mUrl = url;
+        mAuthor = author;
+        mSection = section;
 
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+
+    public void setDate(String date) {
+        mDate = date;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public void setAuthor(String author) {
+        mAuthor = author;
+    }
+
+    public String getSection() {
+        return mSection;
+    }
+
+    public void setSection(String section) {
+        mSection = section;
+    }
+
+    @Override
+    public String toString() {
+        return " title " + getTitle();
+
+    }
 }
--- app/src/main/java/com/example/android/newsapp/NewsAdapter.java
+++ app/src/main/java/com/example/android/newsapp/NewsAdapter.java
@@ -1,8 +1,43 @@
 package com.example.android.newsapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by RO221D on 2018-05-24.
  */
 
-public class NewsAdapter {
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    public NewsAdapter (Context context) {
+        super(context, 0, new ArrayList<News>());
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView title = (TextView) convertView.findViewById(R.id.title);
+        TextView author = (TextView) convertView.findViewById(R.id.author_text_view);
+        TextView section = (TextView) convertView.findViewById(R.id.section_text_view);
+        TextView date = (TextView) convertView.findViewById(R.id.date_text_view);
+
+
+        News currentNews = getItem(position);
+        title.setText(currentNews.getTitle());
+        author.setText(currentNews.getAuthor());
+        date.setText(currentNews.getDate());
+        section.setText(currentNews.getSection());
+
+        return convertView;
+    }
 }
+
